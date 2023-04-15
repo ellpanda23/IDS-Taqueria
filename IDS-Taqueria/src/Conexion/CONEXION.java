bc7bdb23256ba5d880957dc68c6c95d13e5a3b66
Israel Isaac Orozco Gonzalez <75634056+ellpanda23@users.noreply.github.com>
2023-04-15 10:34:11 -0600

    Login, Ordenes, Ticket

---
+++ IDS-Taqueria/src/Conexion/CONEXION.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Brayan
+ */
+public class CONEXION {
+
+    public static Connection conexion, conTrans;
+
+    private static  String a = "jdbc:mysql://localhost:3306/taqueria?user=root&password=root";
+
+    public static boolean conectar() {
+        try {
+            conexion = DriverManager.getConnection(a);
+
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean conectarTrans() {
+        try {
+            conTrans = DriverManager.getConnection(a);
+            conTrans.setAutoCommit(false);
+
+            return true;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static void desconectar() {
+        try {
+            conexion.close();
+        } catch (Exception ex) {
+        }
+    }
+
+}
